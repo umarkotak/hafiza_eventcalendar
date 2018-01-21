0f4e1f5875075c6d920a166a8ee50198e44873b7
umarkotak <umarkotak@yahoo.co.id>
2018-01-21 21:58:26 +0700

    update database

--- latihan.sql
+++ latihan.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 20 Jan 2018 pada 15.16
--- Versi Server: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: Jan 21, 2018 at 03:57 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jadwalku`
+-- Table structure for table `jadwalku`
 --
 
 CREATE TABLE `jadwalku` (
@@ -39,25 +37,23 @@ CREATE TABLE `jadwalku` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `jadwalku`
+-- Dumping data for table `jadwalku`
 --
 
 INSERT INTO `jadwalku` (`jadwal_id`, `date`, `jam_mulai`, `jam_selesai`, `title`, `description`, `id_ruangan`) VALUES
-(3, '2018-01-10 08:28:36', '00:00:00', '00:00:00', 'Ultah MAMA', 'hari ulang tahun mama', 0),
-(6, '2015-01-12 12:11:22', '00:00:00', '00:00:00', 'Tes', 'Tess', 0),
-(7, '2018-01-09 08:28:36', '00:00:00', '00:00:00', 'Sertijab angkatan 2015', 'Serah terima jabatan Koor Laboratorium periode 2016/2018', 0),
-(8, '2018-01-15 00:00:00', '00:00:00', '00:00:00', 'TEST', 'testt', 0),
-(9, '2018-01-16 00:00:00', '00:00:00', '00:00:00', 'TEST2', 'lanjutan test2', 0),
-(10, '2018-01-17 00:00:00', '00:00:00', '00:00:00', 'TEST2', 'cek', 0),
-(11, '2018-01-15 10:55:43', '00:00:00', '00:00:00', 'METLIT', 'Ujian Metlit', 0),
-(12, '2018-01-25 08:04:24', '00:00:00', '00:00:00', 'Test jam', 'jam', 0),
-(13, '2018-01-26 10:55:43', '00:00:00', '00:00:00', 'Rapat Koordinasi', 'Internal Bagian', 0),
-(14, '2018-01-24 00:00:00', '00:00:00', '00:00:00', 'Coba', 'cek', 0);
+(1, '2018-01-21 05:05:00', '05:05:00', '06:06:00', 'sekarang', 'qwerqwewqeqeqw', 1),
+(2, '2018-01-20 05:05:00', '05:05:00', '06:06:00', '1 hari lalu', 'jalan jalan', 1),
+(3, '2018-01-19 05:06:00', '05:06:00', '06:06:00', 'even sesuatu', 'sadsadsdasdsa', 1),
+(4, '2018-01-18 05:05:00', '05:05:00', '06:06:00', 'tanggal 18', 'adwfqfqqwrq', 1),
+(5, '2018-01-17 05:05:00', '05:05:00', '06:06:00', 'tanggal 17', 'jdqwoqpqpowrqwrq', 1),
+(6, '2018-01-22 05:06:00', '05:06:00', '06:06:00', 'besok kita jalan', 'hehe', 1),
+(7, '2018-01-21 13:11:00', '13:11:00', '16:45:00', 'jalan', 'nanti jalan 3', 2),
+(8, '2018-01-23 12:12:00', '12:12:00', '13:12:00', 'tanggal 23', 'qwertyyy', 2);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_ruangan`
+-- Table structure for table `tbl_ruangan`
 --
 
 CREATE TABLE `tbl_ruangan` (
@@ -65,10 +61,18 @@ CREATE TABLE `tbl_ruangan` (
   `nama` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_ruangan`
+--
+
+INSERT INTO `tbl_ruangan` (`id_ruangan`, `nama`) VALUES
+(1, 'lantai 11'),
+(2, 'lantai 12');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_user`
+-- Table structure for table `tbl_user`
 --
 
 CREATE TABLE `tbl_user` (
@@ -80,12 +84,13 @@ CREATE TABLE `tbl_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tbl_user`
+-- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `email`, `status`) VALUES
 (1, 'fizaegyma', 'fizaegyma', 'fizaegyma@gmail.com', 'admin'),
-(2, 'umarkotak', 'umarkotak', 'umarkotak@yahoo.co.id', 'admin');
+(2, 'umarkotak', 'umarkotak', 'umarkotak@yahoo.co.id', 'admin'),
+(3, 'srzky', 'srzky', 'srzky@gmail.com', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -117,21 +122,17 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `jadwalku`
 --
 ALTER TABLE `jadwalku`
-  MODIFY `jadwal_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
+  MODIFY `jadwal_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `tbl_ruangan`
 --
 ALTER TABLE `tbl_ruangan`
-  MODIFY `id_ruangan` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_ruangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
