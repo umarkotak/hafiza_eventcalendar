e5ef1fae19d1edb2a0fef5fd50ecd371437a972e
umarkotak <umarkotak@yahoo.co.id>
2018-01-25 10:26:31 +0700

    change bit header

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <div class="container-fluid">
   <div class="row" style="background-color: white;">
-    <div class="col-md-2">
+    <div class="col-xs-2">
       <img src="image/logo.png" style="width: 100%; margin: 5px;">
     </div>
 
-    <div class="col-md-8">
+    <div class="col-xs-8">
       <h1 style="text-align: center;">EVENT CALENDAR</h1>
     </div>
 
-    <div class="col-md-2">
+    <div class="col-xs-2">
       <img src="image/logo.png" style="width: 100%; margin: 5px;">
     </div>
   </div>
